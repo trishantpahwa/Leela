1e40b9870d3f6790f1ebde3823e21ac2627dfcac
garrud.trishant <garrud.trishant@gmail.com>
2021-02-21 19:03:02 +0530

    Fixed typo or naming a variable in Maarg.

--- src/Maarg.js
+++ src/Maarg.js
@@ -1,66 +1,66 @@
-const router = require('express').Router();
+const router = new require('express').Router();
 
 class Maarg {
-    constructor(raasta, aastha, dhyaan = [], bhakti) {
-        switch (raasta) {
-            case 'ALL':
-                if (dhyaan && dhyaan.length)
-                    router.use(aastha, ...dhyaan, ...bhakti);
-                else router.use(aastha, ...bhakti);
-                break;
-            case 'CONNECT':
-                if (dhyaan && dhyaan.length)
-                    router.connect(aastha, ...dhyaan, ...bhakti);
-                else router.connect(aastha, ...bhakti);
-                break;
-            case 'DELETE':
-                if (dhyaan && dhyaan.length)
-                    router.delete(aastha, ...dhyaan, ...bhakti);
-                else router.delete(aastha, ...bhakti);
-                break;
-            case 'GET':
-                if (dhyaan && dhyaan.length)
-                    router.get(aastha, ...dhyaan, ...bhakti);
-                else router.get(aastha, ...bhakti);
-                break;
-            case 'HEAD':
-                if (dhyaan && dhyaan.length)
-                    router.head(aastha, ...dhyaan, ...bhakti);
-                else router.head(aastha, ...bhakti);
-                break;
-            case 'OPTIONS':
-                if (dhyaan && dhyaan.length)
-                    router.options(aastha, ...dhyaan, ...bhakti);
-                else router.options(aastha, ...bhakti);
-                break;
-            case 'PATCH':
-                if (dhyaan && dhyaan.length)
-                    router.patch(aastha, ...dhyaan, ...bhakti);
-                else router.patch(aastha, ...bhakti);
-                break;
-            case 'POST':
-                if (dhyaan && dhyaan.length)
-                    router.post(aastha, ...dhyaan, ...bhakti);
-                else router.post(aastha, ...bhakti);
-                break;
-            case 'PUT':
-                if (dhyaan && dhyaan.length)
-                    router.put(aastha, ...dhyaan, ...bhakti);
-                else router.put(aastha, ...bhakti);
-                break;
-            case 'TRACE':
-                if (dhyaan && dhyaan.length)
-                    router.trace(aastha, ...dhyaan, ...bhakti);
-                else router.trace(aastha, ...bhakti);
-                break;
-            default:
-                console.warn('Invalid method')
-                break;
-        }
-        return router;
+  constructor(raasta, aastha, dhyaan = [], tapasya) {
+    switch (raasta) {
+      case 'ALL':
+        if (dhyaan && dhyaan.length) {
+          router.use(aastha, ...dhyaan, ...tapasya);
+        } else router.use(aastha, ...tapasya);
+        break;
+      case 'CONNECT':
+        if (dhyaan && dhyaan.length) {
+          router.connect(aastha, ...dhyaan, ...tapasya);
+        } else router.connect(aastha, ...tapasya);
+        break;
+      case 'DELETE':
+        if (dhyaan && dhyaan.length) {
+          router.delete(aastha, ...dhyaan, ...tapasya);
+        } else router.delete(aastha, ...tapasya);
+        break;
+      case 'GET':
+        if (dhyaan && dhyaan.length) {
+          router.get(aastha, ...dhyaan, ...tapasya);
+        } else router.get(aastha, ...tapasya);
+        break;
+      case 'HEAD':
+        if (dhyaan && dhyaan.length) {
+          router.head(aastha, ...dhyaan, ...tapasya);
+        } else router.head(aastha, ...tapasya);
+        break;
+      case 'OPTIONS':
+        if (dhyaan && dhyaan.length) {
+          router.options(aastha, ...dhyaan, ...tapasya);
+        } else router.options(aastha, ...tapasya);
+        break;
+      case 'PATCH':
+        if (dhyaan && dhyaan.length) {
+          router.patch(aastha, ...dhyaan, ...tapasya);
+        } else router.patch(aastha, ...tapasya);
+        break;
+      case 'POST':
+        if (dhyaan && dhyaan.length) {
+          router.post(aastha, ...dhyaan, ...tapasya);
+        } else router.post(aastha, ...tapasya);
+        break;
+      case 'PUT':
+        if (dhyaan && dhyaan.length) {
+          router.put(aastha, ...dhyaan, ...tapasya);
+        } else router.put(aastha, ...tapasya);
+        break;
+      case 'TRACE':
+        if (dhyaan && dhyaan.length) {
+          router.trace(aastha, ...dhyaan, ...tapasya);
+        } else router.trace(aastha, ...tapasya);
+        break;
+      default:
+        console.warn('Invalid method');
+        break;
     }
+    return router;
+  }
 }
 
 module.exports = {
-    Maarg
+  Maarg,
 };
